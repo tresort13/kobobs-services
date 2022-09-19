5d245209d7d64fae5419e6b225be9675bc5e3c62
Mac <mac@MacBook-Pro-de-Mac.local>
2022-09-18 20:17:16 -0700

    first commit

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -108,10 +108,10 @@ function Header(props)
           </Link>
         </Col>
         
-        <Col xs={4} className="my-auto mx-auto text-end justify-content-center">
-         <div className="text-center couleur2 justify-content-center">
-         <div className="couleur2 text-center"><Navbar className="couleur2"><Container><Nav className="couleu2 me-auto"><NavDropdown className="couleur2" title={props.langue} id="basic-nav-dropdown"><NavDropdown.Item href="#action/3.1" className="couleur2">Lingala</NavDropdown.Item><NavDropdown.Item href="#action/3.1" className="couleur2">English</NavDropdown.Item></NavDropdown></Nav></Container></Navbar></div>
-           <i><pre className="couleur2 display-6 timing text-center" style={{fontSize: 15}}>{theTime}</pre></i>
+        <Col xs={4} className="my-auto mx-auto text-end justify-content-end">
+         <div className="text-end couleur2 justify-content-end">
+         <p className="couleur2 text-end"><Navbar className="couleur2" placement="end"><Container><Nav className="couleu2 "><NavDropdown className="couleur2" title={props.langue} id="basic-nav-dropdown"><NavDropdown.Item href="#action/3.1" className="couleur2">Lingala</NavDropdown.Item><NavDropdown.Item href="#action/3.1" className="couleur2">English</NavDropdown.Item></NavDropdown></Nav></Container></Navbar></p>
+           <i><pre className="couleur2 display-6 timing text-end" style={{fontSize: 15}}>{theTime}</pre></i>
         </div>  
         </Col>
     </Row>

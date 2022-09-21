cac669873ff8adf3cfbb42b8ccaa22b28a86bddf
Mac <mac@MacBook-Pro-de-Mac.local>
2022-09-20 22:01:40 -0700

    first commit

--- src/pages/ConfirmationRetraitInfoEnglish.js
+++ src/pages/ConfirmationRetraitInfoEnglish.js
@@ -45,7 +45,7 @@ function ConfirmationRetraitInfoEnglish(props)
     <Row className='justify-content-start pb-3'>
       <hr style={{color:"darkorange"}}></hr>
       {props.envoie2.infoEnvoie.status_retrait == "code retrait en attente de validation" ? <p className='text-dark'>your transfer status : <b className="text-danger"></b> your tracking number validation is still pending...</p> :
-     <p className='text-dark'>your transfer status : <b className="text-success"></b> your tracking number has been activated</p> }
+     <p className='text-dark'><b>your transfer status :</b> <b className="text-success">Your tracking number has been validated</b> </p> }
     </Row>
     <Row className='justify-content-center pb-3' >
 
@@ -96,8 +96,9 @@ function ConfirmationRetraitInfoEnglish(props)
     <Row className='justify-content-start pb-3'>
       <hr style={{color:"darkorange"}}></hr>
       {props.envoie2.infoEnvoie.status_retrait == "code retrait en attente de validation" ? <p className='text-dark'>your transfer status : <b className="text-danger"></b> your tracking number validation is still pending...</p> :
-     <p className='text-dark'>your transfer status : <b className="text-success"></b> your tracking number has been activated</p> }
+     <p className='text-dark'><b>your transfer status :</b> <b className="text-success">Your tracking number has been validated</b> </p> }
     </Row>
+    
     <Row className='justify-content-start pb-3' >
     <Col xs={12}>
         <p className='text-dark'>Tracking Number : <b className='text-danger'> {props.envoie2.infoEnvoie.code_retrait}</b></p>

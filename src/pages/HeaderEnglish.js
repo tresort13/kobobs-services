2379f4f2e22f476785d9d23802020d2452596829
Mac <mac@MacBook-Pro-de-Mac.local>
2023-07-31 01:49:43 -0700

    first commit

--- src/pages/HeaderEnglish.js
+++ src/pages/HeaderEnglish.js
@@ -341,13 +341,13 @@ function HeaderEnglish(props)
    {isMobileOrTablet && <Container className="pt-2" fluid >
 
     <Row className="">
-    <Col xs={2} className="mx-auto my-auto text-start">
+    <Col xs={1} className="mx-auto my-auto text-start">
         <a href="/" style={{textDecoration:"none"}}>
           <Image  src={require('./kobo_logo.JPG')}  className='rounded-pill ' style={{width:80}}></Image>
           </a>
         </Col>
 
-        <Col xs={3} className="pt-4 text-start">
+        <Col xs={4} className="pt-4 text-end">
 <p><Link to='/' style={{textDecoration:"none",fontSize:20}}><b className='couleur2 '><span><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-house-door-fill" viewBox="0 0 16 16">
 <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z"/>
 </svg></span><span >Home</span></b></Link></p>

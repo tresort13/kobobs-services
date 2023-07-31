984bd4a4a4acc48d99025b22ddf1dc1baafee0a5
Mac <mac@MacBook-Pro-de-Mac.local>
2023-07-31 02:01:32 -0700

    first commit

--- src/pages/HeaderEnglish.js
+++ src/pages/HeaderEnglish.js
@@ -341,13 +341,13 @@ function HeaderEnglish(props)
    {isMobileOrTablet && <Container className="pt-2" fluid >
 
     <Row className="">
-    <Col xs={2} className="mx-auto my-auto text-start">
+    <Col xs={1} className="mx-auto my-auto text-start">
         <a href="/" style={{textDecoration:"none"}}>
           <Image  src={require('./kobo_logo.JPG')}  className='rounded-pill ' style={{width:80}}></Image>
           </a>
         </Col>
 
-        <Col xs={2} className="pt-4 text-end">
+        <Col xs={3} className="pt-4 text-end">
 <p><Link to='/' style={{textDecoration:"none",fontSize:20}}><b className='couleur2 '><span>Home</span></b></Link></p>
 </Col>
 
